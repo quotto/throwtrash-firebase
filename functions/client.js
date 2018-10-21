bcd2ad408dc8f044b7c9563945d14607eb804c1f
quotto <rhyme.and.rhyme@gmail.com>
2018-10-22 00:56:04 +0900

    [add] GoogleAssistant向け今日のゴミ出し作成

---
+++ functions/client.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const JSTOffset = 60 * 9 * 60 * 1000; // JST時間を求めるためのオフセット
+
+const TrashType = {
+    burn : 'もえるゴミ',
+    unburn: 'もえないゴミ',
+    plastic: 'プラスチック',
+    bottole: 'ビン<break time="1ms"/>カン',
+    bin: 'ビン',
+    can: 'カン',
+    petbottle: 'ペットボトル',
+    paper: '古紙',
+    resource: '<say-as interpret-as="interjection">資源ゴミ</say-as>',
+    coarse: '<say-as interpret-as="interjection">粗大ゴミ</say-as>'
+};
+
+
+/**
+target_day: 対象とする日を特定するための値。0なら今日、1なら明日……となる。
+**/
+const calculateJSTTime = (target_day) => {
+    var localdt = new Date(); // 実行サーバのローカル時間
+    var jsttime = localdt.getTime() + (localdt.getTimezoneOffset() * 60 * 1000) + JSTOffset + (60 * 24 * 60 * 1000 * target_day);
+    var dt = new Date(jsttime);
+    return dt;
+};
+
+/**
+曜日指定の取得
+現在の曜日と指定の曜日からtarget_dayを算出してgetEnableTrashesを呼び出す
+access_token: ユーザーを特定するためのuuid
+weekday: 指定された曜日 0=日曜日 始まり
+**/
+exports.getEnableTrashesByWeekday = function(data,target_weekday) {
+    const dt = calculateJSTTime(0);
+    const now_weekday = dt.getDay();
+    let target_day = target_weekday - now_weekday;
+    //1より小さい場合は翌週分
+    if(target_day < 1) {
+        target_day += 7;
+    }
+    return this.getEnableTrashes(data,target_day);
+};
+
+/**
+data:   JSON形式のパラメータ
+target_day: チェックするn日目。0なら今日、1なら明日......
+**/
+exports.getEnableTrashes = function(data,target_day) {
+    const result = [];
+    const dt = calculateJSTTime(target_day);
+    data.forEach((trash,index,arr) => {
+        const trash_name = trash['type'] ==='other' ? trash['trash_val'] : TrashType[trash['type']];
+        // const type =  trash['type']
+        trash['schedules'].some(schedule => {
+            if(schedule['type'] === 'weekday') {
+                console.log(dt);
+                if(Number(schedule['value']) === dt.getDay()) {
+                    result.push(trash_name);
+                    return true;
+                }
+            } else if(schedule['type'] === 'biweek') {
+                var matches = schedule['value'].match(/(\d)-(\d)/);
+                var weekday = matches[1];
+                var turn = matches[2];
+
+                // 現在何週目かを求める
+                var nowturn = 0;
+                var targetdate = dt.getDate();
+                while(targetdate > 0) {
+                    nowturn += 1;
+                    targetdate -= 7;
+                }
+
+                if(Number(weekday) === dt.getDay() && Number(turn) === nowturn) {
+                    result.push(trash_name);
+                    return true;
+                }
+            } else if(schedule['type'] === 'month') {
+                if(dt.getDate() === Number(schedule['value'])) {
+                    result.push(trash_name);
+                    return true;
+                }
+            } else if(schedule['type'] === 'evweek') {
+                if(Number(schedule.value.weekday) === dt.getDay()) {
+                    const start_dt = new Date(schedule.value.start);
+                    start_dt.setHours(0);
+                    start_dt.setMinutes(0);
+                    start_dt.setSeconds(0);
+                    start_dt.setMilliseconds(0);
+                    console.log(start_dt);
+
+                    // 今週の日曜日を求める
+                    let current_dt = new Date(dt.toISOString());
+                    current_dt.setHours(0);
+                    current_dt.setMinutes(0);
+                    current_dt.setSeconds(0);
+                    current_dt.setMilliseconds(0);
+                    current_dt.setDate(current_dt.getDate() - current_dt.getDay());
+                    console.log(current_dt);
+
+                    // 登録されている日付からの経過日数を求める
+                    const past_date = (current_dt - start_dt) / 1000 / 60 / 60 / 24;
+                    console.log(past_date);
+
+                    // 差が0またはあまりが0であれば隔週に該当
+                    if(past_date === 0 || (past_date / 7) % 2 === 0) {
+                        result.push(trash_name);
+                        return true;
+                    }
+                }
+            }
+        });
+    });
+    // 同名のゴミがあった場合に重複を排除する
+    return result.filter((value,index,self)=>{
+        return self.indexOf(value) === index;
+    });
+};
---
+++ functions/index.js
@@ -0,0 +1,112 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+const PointDayValue = {
+    '今日':{type:'date',value:0},
+    '明日':{type:'date',value:1},
+    '明後日':{type:'date',value:2},
+    '日曜日':{type:'week',value:0},
+    '月曜日':{type:'week',value:1},
+    '火曜日':{type:'week',value:2},
+    '水曜日':{type:'week',value:3},
+    '木曜日':{type:'week',value:4},
+    '金曜日':{type:'week',value:5},
+    '土曜日':{type:'week',value:6}
+};
+
+const Client = require('./client.js');
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+    const agent = new WebhookClient({ request, response });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+    //   function welcome(agent) {
+    //     // Get the database collection 'dialogflow' and document 'agent' and store
+    //     // the document  {entry: "<value of database entry>"} in the 'agent' document
+    //     const dialogflowAgentRef = db.collection('dialogflow').doc('agent');
+    //     return db.runTransaction(t => {
+    //       t.set(dialogflowAgentRef, {id: '1234',value:'aaaaaaa'});
+    //       return Promise.resolve('Write complete');
+    //     }).then(doc => {
+    //       agent.add(`Wrote to the Firestore database.`);
+    //     }).catch(err => {
+    //       console.log(`Error writing to Firestore: ${err}`);
+    //       agent.add(`Failed to write to the Firestore database.`);
+    //     });
+    //   }
+
+    function get_trashes(agent){
+        const accessToken = agent.user.accessToken;
+        db.collection('schedule').where('id','==',accessToken).get()
+        .then(snapshot => {
+            console.log(`${snapshot[0].id}:${snapshot[0].data}`);
+            const targetDay = agent.parameters.targetDay;
+            let result = 0;
+            if(PointDayValue[targetDay].type === 'date') {
+                result = Client.getEnableTrashes(snapshot[0].data,PointDayValue[targetDay].value)
+            } else {
+                result = Client.getEnableTrashesByWeekday(snapshot[0].data,PointDayValue[targetDay].value)
+            }
+            const speechOut = result.length > 0 ? `${targetDay}出せるゴミは${result.join('、')}、です。` : `${targetDay}出せるゴミはありません。`;
+            agent.add(speechOut);
+            return Promise.resolve(speecOut);
+        }).catch(err => {
+            console.log(err);
+            agent.add('問題が発生しました。開発者へお問い合わせください。');
+        })
+    }
+
+    function fallback(agent) {
+        agent.add(`すみません、わかりません。`);
+        agent.add(`もう一度言っていただけますか？`);
+    }
+
+    // // Uncomment and edit to make your own intent handler
+    // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+    // // below to get this function to be run when a Dialogflow intent is matched
+    // function yourFunctionHandler(agent) {
+    //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+    //   agent.add(new Card({
+    //       title: `Title: this is a card title`,
+    //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+    //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+    //       buttonText: 'This is a button',
+    //       buttonUrl: 'https://assistant.google.com/'
+    //     })
+    //   );
+    //   agent.add(new Suggestion(`Quick Reply`));
+    //   agent.add(new Suggestion(`Suggestion`));
+    //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+    // }
+
+    // // Uncomment and edit to make your own Google Assistant intent handler
+    // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+    // // below to get this function to be run when a Dialogflow intent is matched
+    // function googleAssistantHandler(agent) {
+    //   let conv = agent.conv(); // Get Actions on Google library conv instance
+    //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
+    //   agent.add(conv); // Add Actions on Google library responses to your agent's response
+    // }
+    // // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs/tree/master/samples/actions-on-google
+    // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+
+    // Run the proper function handler based on the matched Dialogflow intent name
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('GetTrashes',get_trashes);
+    // intentMap.set('your intent name here', yourFunctionHandler);
+    // intentMap.set('your intent name here', googleAssistantHandler);
+    agent.handleRequest(intentMap);
+});
